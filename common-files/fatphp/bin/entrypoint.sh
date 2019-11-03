7c81119e5f363761b27d5216a0b62b92f1ebbe2e
Pigo Chu <pigochu@gmail.com>
2019-11-03 12:29:56 +0800

    hidden echo php enable extensions

--- common-files/fatphp/bin/entrypoint.sh
+++ common-files/fatphp/bin/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -f "/root/.fatphp-inited" ] || [ "$PHP_ENABLE_EXTENSIONS" !=  "$(cat /root/.fatphp-inited)" ];
 then
     ## init php extensions
-    echo "Enable php extensions : $PHP_ENABLE_EXTENSIONS"
+    # echo "Enable php extensions : $PHP_ENABLE_EXTENSIONS"
     /opt/fatphp/bin/php-ext-config init
     touch /root/.fatphp-inited
     echo "$PHP_ENABLE_EXTENSIONS" > /root/.fatphp-inited
